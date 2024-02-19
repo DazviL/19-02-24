67c3abbf00cebb9555b82a8f75c056fdbc26555b
DazviL <112430049+DazviL@users.noreply.github.com>
2024-02-19 15:04:31 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -15,14 +15,17 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+          id='lnk'
         >
           Learning React
         </a>
+        <button 
+        onClick={exibirHorario}
+        >Ver Horário</button>
       </header>
     </div>
   );
 }
 
-exibirHorario();
 
 export default App;
